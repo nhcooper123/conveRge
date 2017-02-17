b79522901d056c866447cb055a106e4c10aa8a40
Natalie Cooper <ncooper@tcd.ie>
2017-02-17 15:38:08 +0000

    Add details to pairwise function

--- R/pairwise_converge.R
+++ R/pairwise_converge.R
@@ -1,16 +1,15 @@
-# Required packages
-library(ape)
-library(reshape2)
+# Pairwise phylogenetic/morphological distance test
+# Muschick et al 2012, Current Biology
+# Currently only implements Euclidean distance as in the paper
+# but could incorporate other measures of disparity
 
-# Extract column numbers for a vector of variable names
-id_variables <- function(data, variables) {
-  sapply(variables, function(x)
-         which(names(data) == x))
-}
+# -----------------------------------------------------------------------------
+# Functions to get phylogenetic distances
 
-# Create a dataframe from a phylogenetic distance matrix
+# Create a dataframe from a phylogenetic distance matrix so that it can be
+# easily added to other data for plots and analysis
 # Output columns are the species pairs and the phenetic distance
-# Input is a phylogenetic distance matrix from `cophenetic.phylo`
+# Input is a phylogenetic distance matrix from `ape::cophenetic.phylo`
 get_phylo_dist_data <- function(phylo_matrix) {
   data.frame(expand.grid(speciesA = dimnames(phylo_matrix)[[1]],
 	                       speciesB = dimnames(phylo_matrix)[[2]]),
@@ -18,12 +17,16 @@ get_phylo_dist_data <- function(phylo_matrix) {
 
 }
 
-# Create morphological distance matrix, including only columns containing numbers
+# -----------------------------------------------------------------------------
+# Functions to get morphological distances
+
+# Create morphological distance matrix, including only columns with numbers
+# Uses full triangle of comparisons to ensure it matches to phylogeny
 get_morpho_matrix <- function(data, variable_list) {
   dist(data[variable_list], upper = TRUE, diag = TRUE)
 }
 
-# Flatten morphological distance matrix to add to dataframe
+# Flatten morphological distance matrix so it can be added to a dataframe
 flatten_morpho_matrix <- function(morpho_matrix) {
   melt(as.matrix(morpho_matrix))
 }
@@ -35,25 +38,28 @@ get_flat_morpho_matrix <- function(data, variable_list) {
 
 # Create a dataframe from a flat morphological distance matrix
 # Output columns are the species pairs and the morphological distance
-# Input is a melted morphological distance matrix from `flatten.morpho.matrix`
-# Plus the original morphology dataframe to extract species
+# Input is a melted morphological distance matrix from `flatten_morpho_matrix`
+# Plus the original morphology dataframe to extract species names
 get_morpho_dist_data <- function(data, species_col, flat_morpho_matrix) {
   data.frame(expand.grid(speciesA = data[, species_col],
 	                       speciesB = data[, species_col]),
              morphodist = flat_morpho_matrix$value)
 }
 
+# -----------------------------------------------------------------------------
+# Function to tidy the output
+
 # Merge the phylogenetic and morphological distance matrices
 merge_data <- function(phylo_dist_data, morpho_dist_data) {
   merge(phylo_dist_data, morpho_dist_data, by = c("speciesA", "speciesB"))
 }
 
-# Remove comparisons of species to same species
+# Remove comparisons of species to same species - not relevant for analyses
 remove_intraspecific <- function(merged_data) {
   merged_data[which(merged_data$phylodist != 0 & merged_data$morphodist != 0), ]
 }
 
-# Remove duplicates
+# Remove duplicates - avoids pseudoreplication
 remove_duplicates <- function(merged_data) {
   merged_data[!duplicated(cbind(merged_data$phylodist, merged_data$morphodist)), ]
 }
@@ -65,27 +71,37 @@ tidy_data <- function(phylo_dist_data, morpho_dist_data) {
   tidy <- remove_duplicates(tidy)
 }
 
+# -----------------------------------------------------------------------------
 # Main function
 # Describe in more detail
 
-pairwise <- function(phy, morpho_data, variables, species) {
+pairwise_converge <- function(phy, data, variables, species, 
+                              warn_dropped = FALSE) {
 
 # Check inputs are correct
-if (!is.data.frame(morpho_data))
+if (!is.data.frame(data))
     stop("'data' must be an object of class 'data.frame'")
 
 if (!inherits(phy, "phylo"))
     stop("'phy' must be an object of class 'phylo'")
 
-
-# Check these are all in same order
-# Probably need to order data so it matches the tree
-# Use tree data perhaps?
-
 # Extract list of variables for morphological distances
 # and species names
-variable_list <- id_variables(morpho_data, variables)
-species_col <- id_variables(morpho_data, species)
+variable_list <- id_variables(data, variables)
+species_col <- id_variables(data, species)
+
+# Tidy up data and tree
+# Remove incomplete data, missing species and reorder to match tree
+data <- remove_incomplete_data(data, variable_list)
+data <- remove_missing_species_data(phy, data, species_col)
+phy <- remove_missing_species_tree(phy, data, species_col)
+
+  if (warn_dropped) {
+    tree_not_data <- id_missing_tree(phy, data, species_col)
+    data_not_tree <- id_missing_data(phy, data, species_col)
+  }
+
+data <- sort_species_data(phy, data, species_col)
 
 # Create morphological distance matrix, including only columns with numbers
 flat_morpho_matrix <- get_flat_morpho_matrix(morpho_data, variable_list)
@@ -101,6 +117,8 @@ distances <- tidy_data(phylo_dist_data, morpho_dist_data)
 return(distances)
 
 }
+
+# -----------------------------------------------------------------------------
 
 # Need to run Brownian simulations for comparison
 # Create p value/bootstrapping procedure
